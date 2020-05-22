059373c4de5ec9d38afa7a768bf556d23236d11c
Joel.Garcia <garciajunior796@gmail.com>
2020-05-22 17:04:10 -0500

    método agregar receta ha sido actualizado

--- lib/vistas/mobile/recetas.dart
+++ lib/vistas/mobile/recetas.dart
@@ -61,47 +61,48 @@ class _RecetasState extends State<Recetas> {
     return Container(
       child: SingleChildScrollView(
         padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10.0),
-        child: StreamBuilder(
-          stream: dbProvider.comidaStream,
-          builder: (BuildContext context, AsyncSnapshot<List<Comida>> snapshot){
-            
-            if(!snapshot.hasData) return sinDatos();
+        child: Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  "Mis recetas",
+                  style: TextStyle(fontSize: 20, fontStyle: FontStyle.italic),
+                ),
+                Spacer(),
+                IconButton(
+                  icon: Icon(Icons.add), 
+                  onPressed: (){
+                    Navigator.pushNamed(context, ADDPLATOS);
+                  }
+                )
+              ],
+            ),
+            StreamBuilder(
+              stream: dbProvider.comidaStream,
+              builder: (BuildContext context, AsyncSnapshot<List<Comida>> snapshot){
+                
+                if(!snapshot.hasData) return sinDatos();
 
-            final List<Widget> widgets = new List<Widget>();
+                final List<Widget> widgets = new List<Widget>();
 
-            widgets.add(
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    "Mis recetas",
-                    style: TextStyle(fontSize: 20, fontStyle: FontStyle.italic),
+                widgets.addAll(snapshot.data.map((item)=>ListTile(
+                  leading: Container(
+                    width: MediaQuery.of(context).size.width*0.2,
+                    child: item.urlImagen != null?Image.asset(item.urlImagen):Container()
                   ),
-                  Spacer(),
-                  IconButton(
-                    icon: Icon(Icons.add), 
-                    onPressed: (){
-                      Navigator.pushNamed(context, ADDPLATOS);
-                    }
-                  )
-                ],
-              )
-            );
+                  subtitle: Text(item.descripcion),
+                  title: Text(item.nombre),
+                )).toList());
 
-            widgets.addAll(snapshot.data.map((item)=>ListTile(
-              leading: Container(
-                width: MediaQuery.of(context).size.width*0.2,
-                child: item.urlImagen != null?Image.asset(item.urlImagen):Container()
-              ),
-              subtitle: Text(item.descripcion),
-              title: Text(item.nombre),
-            )).toList());
+                return Column(
+                  children: widgets,
+                );
 
-            return Column(
-              children: widgets,
-            );
-
-          },
+              },
+            ),
+          ],
         )
       ),
       
