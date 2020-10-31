9bab4547485d39b478ba08a839e220b5cf6d5454
Joel.Garcia <garciajunior796@gmail.com>
2020-10-31 15:39:59 -0500

    Fix final weekday

--- lib/providers/db_provider.dart
+++ lib/providers/db_provider.dart
@@ -337,7 +337,7 @@ class DBProvider {
 
     List<AlarmModel> events = new List<AlarmModel>();
 
-    List<AlarmModel> raw = (await db.rawQuery('alarma')).map((i)=>AlarmModel.fromJson(i));
+    List<AlarmModel> raw = (await db.query('alarma')).map((i)=>AlarmModel.fromJson(i)).toList();
 
     raw.forEach((element) {
       if(element.time.weekday == weekday){
