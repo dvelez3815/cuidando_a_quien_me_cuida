83135becdb05e185f25bd89e4cf0f0f3b428baeb
xaviermaster <xavier.velez3132@gmail.com>
2021-01-08 16:28:43 -0600

    insert bd

--- lib/modules/database/database_script.sql
+++ lib/modules/database/database_script.sql
@@ -1,55 +1,85 @@
--- Esta es la única vía por la cuál se pueden agregar números de 
--- menos de 10 dígitos y que no comiencen con 09 o +593, después
--- se puede modificar.
 insert into contacto values
     (0, 'Policía', 'Policía Nacional de Ecuador', '111'),
     (1, 'Emergencia', 'Contacto de emergencia de Ecuador', '911');
 
--- El ID debes ponerlo manualmente, es un entero. Los días vienen dados como un
--- vector con los nombres en español; la hora está en formato 24h. El parámetro
--- 'active' es un booleano, pero como sqlite no lo soporta se pone 1 para true y 0
--- para false, pero ponle siempre 1. El 'type' es el tipo de actividad, en este caso
--- solo acepta 4 posibles valores: mental, recreation, physical, care.
--- Por íltimo están los complementos, que vienen en formato de un array json.
+
 insert into actividad (id, nombre, descripcion, days, time, active, type, complements)
 values
-(0, 'Título ejemplo', 'Descripción ejemplo', 
-    '[lunes, martes, viernes]', '10:30', 1, 'mental', 
-    '[{"title":"nombre material1"},{"title":"nombre material2"}]'
-);
+(0, 'Respiración abdominal', 'La respiración abdominal es fácil de realizar y es muy relajante. Intente este ejercicio básico en cualquier momento que necesite relajarse o aliviar el estrés.', 
+    '[lunes, miercoles, viernes]', '10:30', 3, 'física', 
+'[{}]'),
+(1, 'Respiración 4-7-8', 'Este ejercicio también utiliza la respiración abdominal para ayudarle a relajarse. Puede realizar este ejercicio sentado o recostado.', 
+    '[lunes, miercoles, viernes]', '17:30', 3, 'física', 
+'[{}]'),
+(2, 'Respiración completa', 'El objetivo de la respiración completa es desarrollar el uso completo de los pulmones y centrarse en el ritmo de su respiración. Puede hacerlo en cualquier posición. Pero, mientras aprende, es mejor recostarse boca arriba con las rodillas flexionadas.', 
+    '[lunes, miercoles, viernes]', '20:00', 3, 'física', 
+'[{}]'),
+(3, 'Respiración matinal', 'Intente realizar este ejercicio cuando se levanta por la mañana para aliviar la rigidez de los músculos y liberar las vías respiratorias obstruidas. Luego, utilícelo a lo largo del día para aliviar la tensión en la espalda.', 
+    '[lunes, miercoles, viernes]', '08:00', 3, 'física', 
+'[{}]'),
+(4, 'Meditación', 'Elija un momento y un lugar donde pueda meditar sin interrupciones. Procure buscar un lugar tranquilo, pero no se preocupe si hay algunos ruidos, como el del tráfico. Ese tipo de ruido es simplemente parte del momento presente.', 
+    '[martes, jueves, sabado]', '21:00', 3, 'física', 
+'[{}]'),
+(5, 'TAICHI', 'La especificidad del taichí de bienestar consiste en una ejecución de secuencias hecha con la sensación de llevar unas bolas de ki.', 
+    '[martes, jueves, sabado]', '10:00', 3, 'física', '[{}]'),
 
--- Las alarmas deberás crear una por cada día, y en el parámetro 'day' deberás
--- colocar el número del día, siendo el lunes el día 1. El campo 'interval' hace
--- referencia a cada cuánto tiempo sonará la alarma, por defecto sonará cada semana.
--- 
--- El título y el cuerpo de la alarma será lo que se muestre en la notificación, en
--- la app siempre es el mismo nombre de la actividad a la que pertenece. Se pudo normalizar
--- esto para que esos campos los obtuviese de la tabla actividad mediante joins, pero
--- sqlite es un desastre y daba error, por eso los repetí acá, aunque más adelante se
--- puede normalizar, cuando ya esté todo acabado y haya paciencia para ver por qué dan
--- error los joins.
--- 
--- Lo más probable es que esto se termine normalizando, pero ya después haré un script
--- para que transforme estas sentencias al nuevo modelo de datos.
+(6, 'Bailoterapia', 'La música es la libre expresión de las emociones. La expresión corporal en colectividad puede ayudar a elevar el estado de ánimo, liberar tensiones y motivar al anciano a crear empatía con su entorno.', 
+    '[martes, jueves, sabado]', '14:00', 1, 'recreación', '[{}]'),
+(7, 'Risoterapia', 'Conocida como terapia de risa. Es ideal para liberar tensiones de forma natural usando herramientas como: juegos de mesa, de azar, bailes o chistes.', 
+    '[martes, jueves, sabado]', '7:00', 1, 'recreación', '[{}]'),
+(8, 'Juegos al aire libre', 'Las actividades físicas en exteriores fomentan un envejecimiento sano. A través de movimiento en equipo como: pasar la pelota, mejorar el equilibrio, juego con globos.', 
+    '[lunes, miercoles, viernes]', '7:00', 1, 'recreación', '[{}]'),
+(9, 'Natación', 'Puede aportar no solo ejercicio físico sino, favorecer la energía y vitalidad del adulto. Además, el contacto con el agua puede relajarlo y subir su estado de ánimo.', 
+    '[martes, jueves, sabado]', '8:00', 1, 'bienestar', '[{}]'),
+(10, 'Caminar', 'Ideal para para los que que deseen preservar su masa muscular, mejorar su salud cardiovascular y perder peso como método de prevención a posibles fracturas o lesiones a nivel de vertebras.',
+    '[martes, jueves, sabado]', '13:00', 1, 'bienestar', '[{}]'),
+(11, 'Crucigramas', 'Los juegos con palabras también son de gran ayuda, ya que el lenguaje es un elemento clave en el funcionamiento de los "engranajes" cerebrales.',
+    '[lunes, miercoles, viernes]', '13:00', 1, 'mental', '[{}]'),
+(12, 'Sudokus', 'Los sudokus se popularizaron hace años y se han convertido en una presencia permanente en muchas publicaciones, algo así como la versión numérica de las palabras cruzadas.',
+    '[martes, jueves, sabado]', '17:00', 1, 'mental', '[{}]');
+
+
 insert into alarma (id, title, body, day, time, active, interval)
 values
-(0, 'Título ejemplo', 'descripción ejemplo', 1, "10:30", 1, 7),
-(1, 'Título ejemplo', 'descripción ejemplo', 2, "10:30", 1, 7),
-(2, 'Título ejemplo', 'descripción ejemplo', 5, "10:30", 1, 7);
+(0, 'Juegos al aire libre', 'Las actividades físicas en exteriores fomentan un envejecimiento sano.', 1, "07:00", 1, 7),
+(1, 'Respiración matinal', 'Intente realizar este ejercicio cuando se levanta por la mañana para aliviar la rigidez de los músculos y liberar las vías respiratorias obstruidas. Luego, utilícelo a lo largo del día para aliviar la tensión en la espalda.', 1, "08:30", 1, 7),
+(2, 'Crucigramas', 'Divierte un poco y fortalece tu memoria jugando con crucigramas', 1, "13:00", 1, 7),
+(3, 'Respiración 4-7-8', 'Respiración abdominal para ayudar a relajarse', 1, "17:30", 1, 7),
+(4, 'Respiración completa', 'El objetivo de la respiración completa es desarrollar el uso completo de los pulmones y centrarse en el ritmo de su respiración.', 1, "20:00", 1, 7),
+(5, 'Risoterapia', 'ideal para liberar tensiones de forma natural usando herramientas como: juegos de mesa, de azar, bailes o chistes.', 2, "7:00", 1, 7),
+(6, 'Natación', 'Puede aportar no solo ejercicio físico sino, favorecer la energía y vitalidad del adulto.', 2, "8:00", 1, 7),
+(7, 'TAICHI', 'La especificidad del taichí de bienestar consiste en una ejecución de secuencias hecha con la sensación de llevar unas bolas de ki.', 2, "10:00", 1, 7),
+(8, 'Caminar', 'Ideal para para los que que deseen preservar su masa muscular, mejorar su salud cardiovascular y perder peso como método de prevención a posibles fracturas o lesiones a nivel de vertebras.', 2, "13:00", 1, 7),
+(9, 'Bailoterapia', 'Nada mejor como realiza bailoterapia!', 2, "14:00", 1, 7),
+(10, 'Sudokus', 'Mejora la habilitad mental jugando sodoku', 2, "17:00", 1, 7),
+(11, 'Meditación', 'Procure buscar un lugar tranquilo, pero no se preocupe si hay algunos ruidos, como el del tráfico.', 2, "21:00", 1, 7),
+(12, 'Juegos al aire libre', 'Las actividades físicas en exteriores fomentan un envejecimiento sano.', 3, "07:00", 1, 7),
+(13, 'Respiración matinal', 'Intente realizar este ejercicio cuando se levanta por la mañana para aliviar la rigidez de los músculos y liberar las vías respiratorias obstruidas. Luego, utilícelo a lo largo del día para aliviar la tensión en la espalda.', 3, "08:30", 1, 7),
+(14, 'Crucigramas', 'Divierte un poco y fortalece tu memoria jugando con crucigramas', 3, "13:00", 1, 7),
+(15, 'Respiración 4-7-8', 'Respiración abdominal para ayudar a relajarse', 3, "17:30", 1, 7),
+(16, 'Respiración completa', 'El objetivo de la respiración completa es desarrollar el uso completo de los pulmones y centrarse en el ritmo de su respiración.', 3, "20:00", 1, 7),
+(17, 'Risoterapia', 'ideal para liberar tensiones de forma natural usando herramientas como: juegos de mesa, de azar, bailes o chistes.', 4, "7:00", 1, 7),
+(18, 'Natación', 'Puede aportar no solo ejercicio físico sino, favorecer la energía y vitalidad del adulto.', 4, "8:00", 1, 7),
+(19, 'TAICHI', 'La especificidad del taichí de bienestar consiste en una ejecución de secuencias hecha con la sensación de llevar unas bolas de ki.', 4, "10:00", 1, 7),
+(20, 'Caminar', 'Ideal para para los que que deseen preservar su masa muscular, mejorar su salud cardiovascular y perder peso como método de prevención a posibles fracturas o lesiones a nivel de vertebras.', 4, "13:00", 1, 7),
+(21, 'Bailoterapia', 'Nada mejor como realiza bailoterapia!', 4, "14:00", 1, 7),
+(22, 'Sudokus', 'Mejora la habilitad mental jugando sodoku', 4, "17:00", 1, 7),
+(23, 'Meditación', 'Procure buscar un lugar tranquilo, pero no se preocupe si hay algunos ruidos, como el del tráfico.', 4, "21:00", 1, 7),
+(24, 'Juegos al aire libre', 'Las actividades físicas en exteriores fomentan un envejecimiento sano.', 5, "07:00", 1, 7),
+(25, 'Respiración matinal', 'Intente realizar este ejercicio cuando se levanta por la mañana para aliviar la rigidez de los músculos y liberar las vías respiratorias obstruidas. Luego, utilícelo a lo largo del día para aliviar la tensión en la espalda.', 5, "08:30", 1, 7),
+(26, 'Crucigramas', 'Divierte un poco y fortalece tu memoria jugando con crucigramas', 5, "13:00", 1, 7),
+(27, 'Respiración 4-7-8', 'Respiración abdominal para ayudar a relajarse', 5, "17:30", 1, 7),
+(28, 'Respiración completa', 'El objetivo de la respiración completa es desarrollar el uso completo de los pulmones y centrarse en el ritmo de su respiración.', 5, "20:00", 1, 7),
+(29, 'Risoterapia', 'ideal para liberar tensiones de forma natural usando herramientas como: juegos de mesa, de azar, bailes o chistes.', 6, "7:00", 1, 7),
+(30, 'Natación', 'Puede aportar no solo ejercicio físico sino, favorecer la energía y vitalidad del adulto.', 6, "8:00", 1, 7),
+(31, 'TAICHI', 'La especificidad del taichí de bienestar consiste en una ejecución de secuencias hecha con la sensación de llevar unas bolas de ki.', 6, "10:00", 1, 7),
+(32, 'Caminar', 'Ideal para para los que que deseen preservar su masa muscular, mejorar su salud cardiovascular y perder peso como método de prevención a posibles fracturas o lesiones a nivel de vertebras.',6, "13:00", 1, 7),
+(33, 'Bailoterapia', 'Nada mejor como realiza bailoterapia!', 6, "14:00", 1, 7),
+(34, 'Sudokus', 'Mejora la habilitad mental jugando sodoku', 6, "17:00", 1, 7),
+(35, 'Meditación', 'Procure buscar un lugar tranquilo, pero no se preocupe si hay algunos ruidos, como el del tráfico.', 6, "21:00", 1, 7);
 
--- Finalmente, para acabar con las actividades, hay que agregar la relación, esto en 
--- principio era N a N, pero ya después quedó 1 a N, por lo que esta tabla no es
--- necesaria, y la fk iría en la tabla anterior pero de nuevo, ya después se corrige 
--- esto con algún proceso automatizado.
+
 insert into actividadesAlarmas(alarma_id, actividad_id)
-values (0, 0), (1, 0), (2, 0);
+values (0, 8), (1, 3), (2, 11), (3, 1),(4, 2),(5, 7),(6, 9),(7, 5),(8, 10),(9, 6),(10, 12),(11, 4),(12, 8),(13,3),(14, 11),(15,1),(16, 2),(17, 7),
+(18, 9),(19, 5),(20, 10),(21, 6),(22, 12),(23, 4),(24, 8),(25, 3),(26, 11),(27,1),(28, 2),(29, 7),(30, 9),(31, 5),(32, 10),(33, 6),(34, 12),(35, 4);
 
--- La imágen debe estar en la misma ruta siempre, por lo que la parte de
--- 'assets/imagenes/' es constante. Hay que procurar que las imágenes sean 
--- cuadradas en la medida de lo posible, pero si no se puede no importa
-insert into comida(id, nombre, urlImagen, preparacion, ingredientes)
-values
-(0, 'encebollado', 'assets/imagenes/recetas.jpg', 
-    'Poner pescado en agua hervida con ají peruano, cebolla, y yuca',
-    '[{"title":"3 Yucas"}, {"title":"5 lbs de Pescado"}, {"title":"5 Cebollas"}, {"title":"5 lts de agua"}]'
-);
