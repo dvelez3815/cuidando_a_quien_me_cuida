b72cfc4bc3eba5b9e1774270c53ea46d5c4f2cc2
xaviermaster <40648189+xaviermaster@users.noreply.github.com>
2021-02-15 17:58:23 -0600

    Update nuevo.sql

--- lib/modules/database/nuevo.sql
+++ lib/modules/database/nuevo.sql
@@ -2,19 +2,54 @@ insert into actividad (id, nombre, descripcion, days, time, active, type, comple
 values
 (13, 'Juego De Números', 'Permite trabajar las parte cognitiva de identificar los números, además de reforzar la estabilidad, movilidad y fuerza de tronco y piernas a nivel físico.',
     '[lunes, miercoles, viernes]', '9:00', 0, 'mental', '[]'),
-(14, 'Juego De La Canasta', 'Se estimula a nivel cognitivo la memoria, como el reconocimiento de colores y se trabaja la parte de piernas a nivel físico.',
+(14, 'Juego De Pañuelos', 'Permite agilizar la memoria al recordar los colores y trabajar tanto brazos como piernas a nivel físico.',
+    '[lunes, miercoles, viernes]', '9:00', 0, 'mental', '[]'),    
+(15, 'Juego De La Canasta', 'Se estimula a nivel cognitivo la memoria, como el reconocimiento de colores y se trabaja la parte de piernas a nivel físico.',
     '[lunes, miercoles, viernes]', '14:00', 0, 'mental', '[]'),    
-(15, 'Juego De Movimientos', 'Se estimula la memoria del aspecto cognitivo y la amplitud articular en el aspecto físico.',
+(16, 'Juego De Movimientos', 'Se estimula la memoria del aspecto cognitivo y la amplitud articular en el aspecto físico.',
     '[lunes, miercoles, viernes]', '15:00', 0, 'mental', '[]'),    
-(16, 'Juego De La Baraja', 'Con esta actividad se trabaja la atención divida y selectiva.',
+(17, 'Juego De La Baraja', 'Con esta actividad se trabaja la atención divida y selectiva.',
     '[martes, jueves, sabado]', '15:00', 0, 'mental', '[]'),    
-(17, 'Juego De Encuentra A La Reina', 'Se trabaja y estimula la atención sostenida.',
+(18, 'Juego De Encuentra A La Reina', 'Se trabaja y estimula la atención sostenida.',
     '[martes, jueves, sabado]', '16:00', 0, 'mental', '[]'),        
-(18, 'Juego De Domina El Dominó', 'Se estimulan las funciones ejecutivas, particularmente las memorias de trabajo.',
+(19, 'Juego De Domina El Dominó', 'Se estimulan las funciones ejecutivas, particularmente las memorias de trabajo.',
     '[martes, jueves, sabado]', '17:00', 0, 'mental', '[]'),
-(19, 'Juego Efecto Stroop', 'Se estimulan la inhibición y la atención.',
+(20, 'Juego Efecto Stroop', 'Se estimulan la inhibición y la atención.',
     '[martes, jueves, sabado]', '18:00', 0, 'mental', '[]'),    
-(20, 'Juego De Lectura De Memoria', 'Se estimula la memoria audio verbal y la memoria de trabajo.',
+(21, 'Juego De Lectura De Memoria', 'Se estimula la memoria audio verbal y la memoria de trabajo.',
     '[martes, jueves, sabado]', '19:00', 0, 'mental', '[]'),        
-(21, 'Juego De Secuencias', 'Se estimulan el control mental, ampliando la retentiva de la persona.',
+(22, 'Juego De Secuencias', 'Se estimulan el control mental, ampliando la retentiva de la persona.',
     '[martes, jueves, sabado]', '20:00', 0, 'mental', '[]');
+
+
+insert into procedimiento values
+('***Instrucciones:*** Se colocan los objetos con los números en el piso frente a la persona, esta tendrá que lanzar el dado y dependiendo del número que salga, deberá tocar dicho objeto.
+ ***Observación:*** En caso de no poder agacharse a tocar los objetos, este tendrá que estar sentado alrededor de los mismos.',
+ 6,13),
+ ('***Instrucciones:*** Se debe sentar al paciente y colocarle en cada extremidad un pañuelo, luego se debe decir el nombre de un color y el paciente deberá alzar la extremidad que tenga ese color.
+ ***Observación:*** Con más colores se posean, más difícil se podrá hacer la actividad.',
+ 6,14),
+ ('***Instrucciones:*** Se colocan los pañuelos en una superficie (mesa, silla, entre otros), el cuidador tendrá que decir un color y el paciente deberá tomarlo y ponerlo dentro de la canasta, al final se le pregunta que colores se colocaron en la canasta.
+ ***Observación:*** Con más colores se posean, más difícil se podrá hacer la actividad.',
+ 6,15),
+ ('***Instrucciones:*** Se deberá sentar al paciente y el cuidador/a deberá realizar una serie de movimientos como por ejemplo, alzar un brazo, aplaudir, girar la cabeza, entre otros (queda a creatividad del cuidador), y el paciente deberá repetirlos. Todo esto en un orden empezando con 3 movimientos y aumentado la dificultad poco a poco.
+ ***Observación:*** Realizar movimientos en base a la movilidad del paciente.',
+ 6,16),
+ ('***Instrucciones:*** Se procede a decirle que recuerde una carta al paciente, después se esconden las cartas en los vasos mostrándole donde va cada una y se comienzan a mover durante 30 segundos, finalmente se le pide que seleccione el vaso en donde está la carta mostrada al principio, o puede ir volteando los vasos en orden descendente o ascendente de las cartas.
+ ***Observación:*** Se puede aumentar la dificultad pidiendo que recuerde las demás cartas.',
+ 6,17),
+ ('***Instrucciones:*** Se toman 3 naipes y se le muestran al paciente donde uno de ellos será la reina de corazones (el naipe con la letra Q) y luego se comienza a revolver durante 30 segundos o más, al final se le pide que muestre donde se quedó la reina de corazones y sino adivina, se vuelve a repetir el proceso.
+ ***Observación:*** Se trabaja y estimula la atención sostenida.',
+ 6,18),
+ ('***Instrucciones:*** Se colocan 5 fichas de dominó o más boca abajo en fila, luego se procede a mostrar la primera y se la vuelve a dejar como estaba, después se procede a mostrar la segunda y se le pregunta la ¿Cuántos puntos tenía la ficha anterior?, así sucesivamente hasta terminar de mostrar todas las fichas, en caso de que falle, vuelve a repetir el juego.
+ ***Observación:*** Si quiere aumentar la dificultad, puede mostrar más de una ficha en orden y luego le pregunta el orden.',
+ 6,19),
+ ('***Instrucciones:*** Se muestran una a una las tarjetas al paciente y se le dice que mencione el color de la palabra, sin leer la palabra.
+ ***Observación:*** Se puede invertir el juego pidiendo que diga lo escrito y no el color.',
+ 6,20),
+ ('***Instrucciones:*** Se le pedirá que recuerde la última palabra de cada frase dicha y se le procede a leer, una vez terminada la lectura, se le pregunta que palabra fue la última mencionada.  
+ ***Observación:*** Si el ejercicio es muy fácil puede extender la lectura o pedirle al paciente.',
+ 6,21),
+ ('***Instrucciones:*** Se le dice al paciente que diga una serie de números o palabras, luego que las haya dicho, se le pide que las vuelva a repetir en orden inverso, así sucesivamente aumentando la dificultad.  
+ ***Observación:*** Se recomienda empezar con una serie de 3 números o palabras.',
+ 6,22);
